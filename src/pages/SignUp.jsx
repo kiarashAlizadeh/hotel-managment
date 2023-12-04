b8209ced156ffe0042fe0fb7ccf21b302a0b91dc
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-12-04 11:13:35 +0330

    test loading in react

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react"
+
 import MyNavbar from "../components/MyNavbar"
 import Footer from "../components/Footer"
 import SignUpCom from "../components/SignUpCom"
 
 function SignUp() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
   return (
     <>
-      <MyNavbar />
-      <SignUpCom />
-      <Footer />
+      {isLoading ? (
+        <p> Loading ...</p>
+      ) : (
+        <>
+          <MyNavbar />
+          <SignUpCom />
+          <Footer />
+        </>
+      )}
     </>
   )
 }
