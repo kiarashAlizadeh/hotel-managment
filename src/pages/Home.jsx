d8744cf9e9cb5cb6d57a3687a0f396036d473f0d
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2023-12-04 12:16:32 +0330

    test loading in react4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,25 @@ import MyNavbar from "../components/MyNavbar"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 
+// Loader import
+import useLoading from "../hooks/useLoading"
+import Loading from "../components/loading/Loading"
+
 function Home() {
+  // Loader
+  const isLoading = useLoading()
   return (
-    <div className="flex h-screen flex-col justify-between">
-      <MyNavbar />
-      <Header />
-      <Footer />
-    </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="flex h-screen flex-col justify-between">
+          <MyNavbar />
+          <Header />
+          <Footer />
+        </div>
+      )}
+    </>
   )
 }
 
